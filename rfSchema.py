8ca6bd1d31bce37cca93c0f8c2278952696eb04b
Tomas Gonzalez <tomas.g.2012@gmail.com>
2019-01-31 14:54:24 -0600

    Updated schema_pack, fix on revisions

--- rfSchema.py
+++ rfSchema.py
@@ -674,9 +674,14 @@ def getPropertyDetails(schemaObj, propertyOwner, propertyName, val, topVersion=N
                 ['NavigationProperty', 'Property'], attrs={'Name': propertyName}, recursive=False)  # BS4 line
 
             # start adding attrs and props together
-            propertyInnerTags = propertyTag.find_all()  # BS4 line
+            propertyInnerTags = propertyTag.find_all(recursive=False)
             for tag in propertyInnerTags:
-                propEntry[tag['Term']] = tag.attrs
+                if(not tag.get('Term')):
+                    rst.traverseLogger.warn(tag, 'does not contain a Term name')
+                elif (tag.get('Term') == 'Redfish.Revisions'):
+                    propEntry[tag['Term']] = tag.find_all('Record')
+                else:
+                    propEntry[tag['Term']] = tag.attrs
             propertyFullType = propertyTag.get('Type')
         else:
             propEntry['isTerm'] = True
